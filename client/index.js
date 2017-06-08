c92efb4a1ba776910d4955d482da953f1afc7c9d
Coder Vince <publicvince102@gmail.com>
2017-06-08 04:17:15 +0200

    webpack setup

---
+++ client/index.js
@@ -0,0 +1,6 @@
+// react app
+ReactDOM.render(
+  // React.createElement("h2", null, "Hello react"),
+  <h2> Hello React with JSX! </h2>,
+  document.getElementById("root")
+)
--- client/script.js
+++
@@ -1,5 +0,0 @@
-// react app
-ReactDOM.render(
-  React.createElement("h2", null, "Hello react"),
-  document.getElementById("root")
-)
