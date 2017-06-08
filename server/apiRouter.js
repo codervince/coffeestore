88b844c4ec5f16ef3c3b3323debbcc783fa9a971
Coder Vince <publicvince102@gmail.com>
2017-06-08 10:26:10 +0200

    heroku up and running

--- server/apiRouter.js
+++ server/apiRouter.js
@@ -6,12 +6,22 @@ const router = express.Router();
 
 const pg = require('pg');
 
+// const pool = new pg.Pool({
+//   database: 'coffee-dev',
+//   user: 'postgres', //ENV var PGUSER
+//   password: 'I17nvader' //ENV var PGPASSWORD
+// });
+
 const pool = new pg.Pool({
-  database: 'coffee-dev',
-  user: 'postgres', //ENV var PGUSER
-  password: 'I17nvader' //ENV var PGPASSWORD
+  database: 'd92m039fkfvb02',
+  user: 'npgdgpsnmawanl', //ENV var PGUSER,
+  host: 'ec2-107-20-226-93.compute-1.amazonaws.com',
+  password: '52434dcc41da873ee69a97cdca433c71719bce23c583d85c82b3f5ebd2cbc794', 
+  ssl:true
 });
 
+// from heroku config
+//postgres://npgdgpsnmawanl:52434dcc41da873ee69a97cdca433c71719bce23c583d85c82b3f5ebd2cbc794@ec2-107-20-226-93.compute-1.amazonaws.com:5432/d92m039fkfvb02
 
 router.get('/coffees', (req, res) => {
 
